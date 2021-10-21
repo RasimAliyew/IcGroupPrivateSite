916e95888e81ff0c0eed1e073185c1a63952e6b2
Rasim <allen.wallker.00@bk.ru>
2021-10-21 21:45:41 +0400

    First Form

---
+++ 1C_Group/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace _1C_Group.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
---
+++ 1C_Group/obj/Release/netcoreapp3.1/PubTmp/Out/wwwroot/js/site.js
@@ -0,0 +1,19 @@
+﻿let count = 0;
+function myFunction(x) {
+    count++;
+    x.classList.toggle("change");
+    if (count % 2 != 0) {
+        document.querySelector('.LeftPage').style.display = "flex";
+        document.querySelector('.LeftPage').style.width = "100%";
+        document.querySelector('.CenterItems').style.display = "none";
+    }
+    else {
+        document.querySelector('.LeftPage').style.display = "none";
+        document.querySelector('.CenterItems').style.display = "flex";
+
+    }
+
+}
+
+
+
